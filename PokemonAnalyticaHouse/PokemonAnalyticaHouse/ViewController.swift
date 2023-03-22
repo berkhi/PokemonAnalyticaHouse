3e9aa118e467a59481ce18c2da3e75effcf7dcc3
Berk Hayta <berkhayta@hotmail.com>
2023-03-22 17:31:22 +0300

    Initial commit

---
+++ PokemonAnalyticaHouse/PokemonAnalyticaHouse/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonAnalyticaHouse
+//
+//  Created by BerkH on 22.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
